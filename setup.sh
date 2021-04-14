84671e97284d7144dfca9e5c7b2d759791ead0ca
JT Archie <jtarchie@gmail.com>
2021-04-14 11:49:08 -0600

    fix typo

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ set -eux
 
 dotfiles="$PWD"
 # setup git to be ergonomic
-if grep "$dotfiles/.git-config" -q -f "$HOME/.gitconfig" ; then
-    cat "$dotfiles/.git-config" >> "$HOME/.gitconfig"
+if grep "$dotfiles/.gitconfig" -q -f "$HOME/.gitconfig" ; then
+    cat "$dotfiles/.gitconfig" >> "$HOME/.gitconfig"
 fi
 
 sudo apt-get install -y ripgrep
