0f3ea456803cb45f2596adfb8fb30a36370af5be
JT Archie <jtarchie@gmail.com>
2021-04-14 11:19:16 -0600

    remove line

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ set -eux
 
 dotfiles="$PWD"
 # setup git to be ergonomic
-ln -sf "$dotfiles/.git-authors" "$HOME/.git-authors"
 if grep "$dotfiles/.git-config" -q -f "$HOME/.gitconfig" ; then
     cat "$dotfiles/.git-config" >> "$HOME/.gitconfig"
 fi
