b0dcacab1290fc209ddc04f28ddd43945e172895
JT Archie <jtarchie@gmail.com>
2021-05-25 14:14:38 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ if grep "$dotfiles/.gitconfig" -q -f "$HOME/.gitconfig" ; then
     cat "$dotfiles/.gitconfig" >> "$HOME/.gitconfig"
 fi
 
-# sudo add-apt-repository -y ppa:longsleep/golang-backports
-# sudo apt update -y
-# sudo apt install -y golang-go
+sudo add-apt-repository -y ppa:longsleep/golang-backports
+sudo apt update -y
+sudo apt install -y golang-go
 sudo apt-get install -y ripgrep
 
-# go get -u github.com/boyter/scc/
-# export PATH=$PATH:$HOME/go/src/
+go get -u github.com/boyter/scc/
+export PATH=$PATH:$HOME/go/src/
